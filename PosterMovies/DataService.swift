c4989df0b31bc900d0644078f36249af976d28e3
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 08:25:37 -0300

    feat: create data service to mock data

--- PosterMovies/DataService.swift
+++ PosterMovies/DataService.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct DataService {
+    func getData() -> [MoviePosterItem] {
+        return [MoviePosterItem(name: "Captain Ameria", imageName: "captainamerica"),
+                MoviePosterItem(name: "Dune", imageName: "dune"),
+                MoviePosterItem(name: "Home Alone", imageName: "homealone"),
+                MoviePosterItem(name: "Mario Bros", imageName: "mariobros"),
+                MoviePosterItem(name: "Mission Impossible", imageName: "missionimpossible"),
+        ]
+    }
+}
