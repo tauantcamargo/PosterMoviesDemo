9168bb00dfa49a358bc2610067664f9383e2c15c
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 01:36:28 -0300

    Initial Commit

---
+++ PosterMovies/PosterMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosterMoviesApp.swift
+//  PosterMovies
+//
+//  Created by Tauan Tathiell Camargo on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PosterMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
